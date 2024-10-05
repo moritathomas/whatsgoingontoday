34e37a5323f7835681cd22c38537c3f223424b6d
Thomas Morita <thomasmorita@Thomass-MacBook-Air.local>
2024-10-04 23:04:18 -0400

    pull on news

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
 # main.py
 from fastapi import FastAPI
+import logging
+from services.news import get_news
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
+
+
+# Set up basic logging configuration
+logging.basicConfig(level=logging.INFO)
+
+# Create a logger
+logger = logging.getLogger(__name__)
+
+
 
 app = FastAPI()
 
+
+# Allow CORS for the frontend app
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # URL of your React app
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
 @app.get("/")
 def read_root():
-    return {"whatsgoinontoday": "World"}
+    msg = get_news()
+    return msg
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
     return {"item_id": item_id}
 
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
