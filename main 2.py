ed7fcea39206f519b0f598a9e8a110922c92acdd
Thomas Morita <thomasmorita@Thomass-MacBook-Air.local>
2024-10-04 20:24:44 -0400

    asdf

--- main 2.py
+++ main 2.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"whatsgoinontoday": "World"}
+    return {"Hello": "World"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
